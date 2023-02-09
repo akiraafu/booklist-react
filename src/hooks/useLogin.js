9ce43bce3255958a94067400a654dc4b33fd27ce
Junrong Fu <junrongfu6@gmail.com>
2023-02-09 22:03:08 +0800

    commit

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,15 +1,18 @@
 import { useState } from "react";
-import { auth } from "../firebase/config";
+import { useAuthContext } from "./useAuthContext";
 
+//firebase imports
+import { auth } from "../firebase/config";
 import { signInWithEmailAndPassword } from "firebase/auth";
 
 export const useLogin = () => {
   const [error, setError] = useState(null);
+  const { dispatch } = useAuthContext();
   const login = (email, password) => {
     setError(null);
     signInWithEmailAndPassword(auth, email, password)
       .then((res) => {
-        console.log("user logged in:", res.user);
+        dispatch({ type: "LOGIN", payload: res.user });
       })
       .catch((err) => {
         setError(err.message);
